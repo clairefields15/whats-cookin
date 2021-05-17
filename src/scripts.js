95a8353f3a51095e13f7a6051840223573343e02
darlaevans2000 <darlaevans2000@gmail.com>
2021-05-17 15:28:18 -0600

    Finish up basic html

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,45 @@
-import './styles.css';
-import apiCalls from './apiCalls';
+const homePage = document.getElementById('homePage');
+const queuePage = document.getElementById('queuePage');
+const favoritesPage = document.getElementById('favoritesPage');
+const recipeDetailPage = document.getElementById('recipeDetailPage');
+const searchResultsPage = document.getElementById('searchResultsPage');
+
+const homeButton = document.getElementById('homeButton');
+const favoriteButton = document.getElementById('favoriteButton');
+const queueButton = document.getElementById('queueButton');
+
+const browseMeals = document.getElementById('browseMeals');
 
 
+function hide(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    element = elements[i];
+    element.classList.add("hidden");
+  }
+};
 
+function show(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    element = elements[i];
+    element.classList.remove("hidden");
+  }
+};
 
-console.log('Hello world');
+function goHome() {
+  show([homePage, browseMeals]);
+  hide([recipeDetailPage, favoritesPage, searchResultsPage, queuePage]);
+}
+
+function displayQueue() {
+  show([queuePage]);
+  hide([homePage, recipeDetailPage, favoritesPage, searchResultsPage, browseMeals]);
+}
+
+function displayFavorites() {
+  show([favoritesPage]);
+  hide([homePage, searchResultsPage, recipeDetailPage, browseMeals, queuePage]);
+}
+
+homeButton.addEventListener('click', goHome);
+favoriteButton.addEventListener('click', displayFavorites);
+queueButton.addEventListener('click', displayQueue);

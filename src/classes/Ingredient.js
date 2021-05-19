0b0a5cdbee39ad373062d30acb3359e35231ec7f
darlaevans2000 <darlaevans2000@gmail.com>
2021-05-18 20:02:23 -0600

    Add getIngredients and getRecipeCost methods

--- src/classes/Ingredient.js
+++ src/classes/Ingredient.js
@@ -2,7 +2,9 @@ class Ingredient {
   constructor(ingredientData) {
     this.id = ingredientData.id;
     this.name = ingredientData.name;
-    this.cost = ingredientData.cost;
+    this.estimatedCostInCents = ingredientData.estimatedCostInCents;
+    this.amount = ingredientData.quantity.amount;
+    this.unit = ingredientData.quantity.unit;
   }
 }
 
--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,7 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Recipe = require('../src/classes/Recipe')
+import Recipe from '../src/classes/Recipe'
+import Ingredient from '../src/classes/Ingredient'
 
 describe('Recipe', () => {
   let recipe, ingredient;
@@ -38,8 +39,10 @@ describe('Recipe', () => {
       tags: ["starter", "snack", "appetizer"]
     };
 
+    const ingredientData = {id: 20081, name: "wheat flour", cost: 142}
+
     recipe = new Recipe(recipeData);
-    ingredient = new Ingredient()
+    //ingredient = new Ingredient(ingredientData)
   });
 
   it('should instantiate a new Recipe', () => {
@@ -99,14 +102,32 @@ describe('Recipe', () => {
     expect(recipe.tags).to.deep.equal(tags)
   });
 
-  it('should have a way to determine the names of ingredients needed', () => {
-    const ingredients = recipe.ingredientsNeeded();
-    expect(ingredients).to.equal()
-
+  it('should have a way to return the names of ingredients needed', () => {
+    const ingredients = recipe.getIngredients();
+    const answer = [
+      {
+        id: 20081,
+        name: 'wheat flour',
+        estimatedCostInCents: 142,
+        amount: 1.5,
+        unit: 'c'
+      },
+      {
+        id: 18372,
+        name: 'bicarbonate of soda',
+        estimatedCostInCents: 582,
+        amount: 0.5,
+        unit: 'tsp'
+      }
+    ]
+    expect(ingredients).to.deep.equal(answer)
   })
 
-  it.skip('should be able to get the cost of the ingredients', () => {
-
+  it.only('should be able to get the cost of the ingredients', () => {
+    const cost = recipe.getRecipeCost();
+    const answer = "$5.04"
+    
+    expect(cost).to.equal(answer)
   })
 
   it.skip('should return it/s instructions', () => {

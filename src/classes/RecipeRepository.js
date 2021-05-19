8e614a128411720ac41cda2239489bc8d2aaa8ef
Claire Fields <clairefields15@gmail.com>
2021-05-19 15:36:43 -0600

    Make data file for testing and start recipe repository:

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,7 +1,11 @@
+import Recipe from './Recipe';
+import Ingredient from './Ingredient';
+import { ingredientsData } from "../data/ingredients";
+import { recipeData } from "../data/recipes";
+import { recipeTestData } from '../data/recipe-test-data'
 class RecipeRepository {
   constructor() {
 
-    // One class to get you started!
   }
 }
 
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,27 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import Ingredient from '../src/classes/Ingredient';
+import { ingredientsData } from '../src/data/ingredients';
+import { recipeData } from '../src/data/recipes';
+import { recipeTestData } from '../src/data/recipe-test-data'; 
+import Recipe from '../src/classes/Recipe';
 
 describe('RecipeRepository', () => {
+  let recipeRepository, recipe1, recipe2, recipe3;
+
+  beforeEach(() => {
+    recipeRepository = new RecipeRepository();
+    recipe1 = new Recipe(recipeData[0]);
+    recipe2 = new Recipe(recipeData[1]);
+    recipe3 = new Recipe(recipeData[2]);
+  });
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+
+  it('should have a parameter of recipe data', () => {
+
+  })
+
 })

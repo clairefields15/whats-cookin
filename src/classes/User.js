14e3712bf3c9067f4960fe14a5e4c1bec4a424a7
Claire Fields <clairefields15@gmail.com>
2021-05-21 11:46:32 -0600

    Add name and id property to test/implementation

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,6 +1,7 @@
 class User {
-  constructor() {
-
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
   }
 
 }
--- test/User-test.js
+++ test/User-test.js
@@ -4,12 +4,24 @@ import User from '../src/classes/User';
 import { userData } from '../src/data/users';
 
 describe('User', () => {
-  
+  let user;
+
+  beforeEach(() => {
+    user = new User("Saige O'Kon",1)
+  })
+
   it('should instantiate a new User', () => {
-    const user = new User()
     expect(user).to.be.an.instanceOf(User)
   })
 
+  it('should have a name', () => {
+    expect(user.name).to.equal("Saige O'Kon")
+  })
+
+  it('should have an id', () => {
+    expect(user.id).to.equal(1)
+  })
 
 
+//id, pantry, favoriteRecipes, recipesToCook, ...more?
 });
